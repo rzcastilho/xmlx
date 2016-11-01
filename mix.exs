7ae19488a9074f7cd766885d7e9f1e0842695280
Rodrigo Zampieri Castilho <castilho@Rodrigos-MacBook-Pro-2.local>
2016-11-01 01:12:15 -0200

    Fix mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,8 @@
 defmodule Xmlx.Mixfile do
   use Mix.Project
 
-<<<<<<< HEAD
   @description "Elixir native XML parser that enables search using attribute or element names"
   @version "0.1.0"
-=======
-  @description "Xmlx is a XML parser that enables search using attribute or element names."
-  @version "0.0.2"
->>>>>>> origin
 
   def project do
     [app: :xmlx,
